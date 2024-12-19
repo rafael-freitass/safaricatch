38cf059f369b26af3c8cddff04d4ef663fd5f99c
Yasmim <108767728+Tybika@users.noreply.github.com>
2024-12-19 01:42:45 -0300

    feat(wip): Defines 'instrucoes' screen requirements and style

---
+++ src/modules/instrucoes/instrucoes.py
@@ -0,0 +1,13 @@
+# Importações do projeto
+import text_functions
+
+# Geração tela
+
+# estilo
+# titulo; centralizado
+# caixa: controles: tecla, texto; 1 por linha, centralizado
+# texto com instruções justificado
+
+# Testes
+if __name__ == "__main__":
+    pass
