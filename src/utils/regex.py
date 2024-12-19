4c9b4cc5f2c75513f0e574971619212a71bb48aa
Yasmim <108767728+Tybika@users.noreply.github.com>
2024-12-19 18:48:25 -0300

    feat: Includes 'silaba' to regex compilations.
    Termina a compilação de 'silaba' e a documenta.
    Corrige erros de sintaxe em outras compilações.

--- src/utils/regex.py
+++ src/utils/regex.py
@@ -16,21 +16,34 @@ consoantes_finais = re.compile(r'[lmnrsLMNRS]')
 # Verifica se próximo char não é uma vogal
 n_vogal = re.compile(r'(?!'+ todas_vogais.pattern +r')')
 
-# Verifica se próximo char não é l ou n
+# Verifica se próximo char não é l nem n
 n_ln = re.compile(r'(?![lnLN])')
 
 # Compila consoantes e verificação de vogal
-consoantes_pnv = re.compile(r'('+ consoantes_plural + n_vogal +r')')
-consoantes_fnv = re.compile(r'('+ consoantes_finais + n_vogal +r')')
+consoantes_pnv = re.compile(r'('+ consoantes_plural.pattern + n_vogal.pattern +r')')
+consoantes_fnv = re.compile(r'('+ consoantes_finais.pattern + n_vogal.pattern +r')')
 
 # Expressão que define sílabas
 ## padrões: vogal; consoante + vogal
-## parte do mais específico até o mais geral, 
-## operador | exclusivo
+## inicia mais específico vai até mais geral, devido operador | exclusivo
 silaba = re.compile(
-    r'('+ consoantes_inseparaveis.pattern +r'|'
-    + todas_consoantes.pattern +r'|'+ todas_vogais +r'|ç)('
-    r'(u'+ vogais_nu.pattern + semivogais.pattern + consoantes_pnv.pattern +r')|'
-    ++r')'
-) 
-## @todo transcrever ditongos em diante, revisar documentação
+    r'(' # Primeira parte da sílaba 
+    # encontros consonantais ou
+    + consoantes_inseparaveis.pattern + r'|' 
+    # consoantes solo (exceto ç) ou ç ou
+    + todas_consoantes.pattern + r'|[çÇ]|'
+    # vogais solo
+    + todas_vogais.pattern + r')'
+
+    r'(' # Segunda parte da sílaba
+    # tritongo com consoante opcional que estende o som ou
+    + r'(u'+ vogais_nu.pattern + semivogais.pattern + consoantes_pnv.pattern +r'?)|('
+    # ditongo não-iniciado em u com plural opcional ou
+    + vogais_nu.pattern + r'('+ semivogais.pattern + n_ln.pattern + r')([mrsMRS]'+ n_vogal.pattern + r')?)|'
+    # ditongo(opcional) vogal com consoante opcional que estende o som ou
+    + r'(u?'+ vogais_nu.pattern + consoantes_fnv.pattern +r'?)|('
+    # consoantes 
+    + consoantes_fnv.pattern + r'))', 
+    flags=re.VERBOSE) 
+
+
