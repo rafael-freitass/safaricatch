bc41a939b893c2113c57184a3cee7650a3caf74e
Yasmim <108767728+Tybika@users.noreply.github.com>
2025-01-07 17:45:20 -0300

    feat: Implements 'Funções de geração da tela'
    Implementa novas funções, melhorias no texto e espaçamento da documentação

--- src/modules/instrucoes/text_functions.py
+++ src/modules/instrucoes/text_functions.py
@@ -3,17 +3,21 @@ import sys
 import os
 import re
 
+
 # Adiciona caminho para 'utils' na busca de módulos
 sys.path.append(os.path.abspath(os.path.join(
     os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'utils')))
+
 
 # Importações do projeto
 from regex import palavra, silaba, pontuacao
 from main import a, b   # @todo mudar essa importação quando alguma página definir o tamanho da matriz
 
+
 # Globais
 _tamanho_mod_ = [a, b]
 _TAMANHO_ = tuple(_tamanho_mod_)
+
 
 # Funções de formatação de texto
 ## Retorna altura e largura "livres" de um container regular
@@ -38,7 +42,7 @@ def calcular_capacidade(matriz):
     
     return capacidade
 
-## Retorna True se a string passada é maior que o tamanho da matriz
+## Retorna True se a string passada é maior que o tamanho da linha
 def verificar_str_maior(string):
     global _tamanho_mod_
     if (len(string) <= _tamanho_mod_[1]):
@@ -46,8 +50,18 @@ def verificar_str_maior(string):
     
     return True
 
+## Retorna True se a matriz é maior do que a tela verticalmente 
+def verificar_matriz_maior(matriz):
+    global _tamanho_mod_
+    if (len(matriz) <= _tamanho_mod_[0]):
+        return False
+    
+    return True
+
 ## Retorna uma lista de strings, separador: última sílaba cabível
 def separar_silaba(string):
+    #@todo tratar exceções (palavras com hífen, frases terminadas em espaço)
+    #@todo fix: hífens inseridos pela função não estão aparecendo
     global _tamanho_mod_
     if (verificar_str_maior(string)):
         # Encontra posição do último espaço entre tamanho mínimo e máximo
@@ -79,28 +93,28 @@ def separar_silaba(string):
         return string_mod
     return ([string])
 
-## Diminui espaço para escrita na tela, reseta margem anterior
+## Diminui _tamanho_mod_, reseta margem anterior, retorna valor de entrada
 def alinhar_add_margem_tela(integer):
     global _tamanho_mod_, _TAMANHO_
 
     if (integer < (_TAMANHO_[0]/2) and integer < (_TAMANHO_[1]/2)):
         for i in range(len(_TAMANHO_)):
             _tamanho_mod_[i] = _TAMANHO_[i] - (2*integer)
-    return
+    
+    return integer
 
-## Alinha no centro com base no tamanho total
+## Retorna string alinhada ao centro com base no tamanho da tela
 def alinhar_centro(string):
     global _TAMANHO_
-    string.center(_TAMANHO_[1])
-    pass
+    return (string.center(_TAMANHO_[1]))
 
-## Alinha à margem esquerda
+## Altera string para alinhada à esquerda com base no _tamanho_mod_
 def alinhar_esquerda(string):
     global _tamanho_mod_
     string.ljust(_tamanho_mod_[1])
     pass
 
-## Alinha à margem direita
+## Altera string para alinhada à direita com base no _tamanho_mod_
 def alinhar_direita(string):
     global _tamanho_mod_
     string.rjust(_tamanho_mod_[1])
@@ -151,6 +165,8 @@ def alinhar_justificar(lista_strings):
     return lista_strings
 
 
+
+
 # Funções de geração da tela
 ## Retorna uma matriz geradora de um retângulo com as dimensões passadas
 def container_retangulo(altura, largura):
@@ -186,7 +202,18 @@ def container_retangulo(altura, largura):
                     m[i].append(' ')
     return m
 
-## Insere texto em um container existente
+## Retorna uma matriz geradora de uma caixa de texto com as dimensões passadas
+def container_textbox(altura, largura):
+    m = []
+    for i in range(altura):
+        m.append([])
+
+        for j in range(largura):
+            m[i].append(' ')
+
+    return m
+
+## Insere texto em um container existente, retorna o container modificado
 def container_text(matriz_container, string):
     # Armazena valor original de _tamanho_mod e redefine
     global _tamanho_mod_
@@ -195,60 +222,136 @@ def container_text(matriz_container, string):
     
     # Inicialização das variáveis de controle
     lista_string = separar_silaba(string)
-    i = 1
+    i = 0
     s = 0
-    
+
     # Laço transcrevendo no container
     while i < len(lista_string):
-        lista_string = lista_string[:i] + separar_silaba(lista_string[i])
+        if (not(i == 0)):
+            lista_string = lista_string[:i] + separar_silaba(lista_string[i])
         l = 0
-        
         for j in range(len(matriz_container[i])):
             if (matriz_container[i][j] == ' '):
                 if (l < len(lista_string[s])):
                     matriz_container[i][j] = lista_string[s][l]
                     l += 1
-        s += 1
+
+        if not(l == 0):
+            s += 1
         i += 1
-    # Repetição perdida pela inicialização fora do laço
-    l = 0
-    for j in range(len(matriz_container[i])):
+    
+    # Repetição perdida pela inicialização de lista_string
+    if (s < len(lista_string)):
+        l = 0
+        for j in range(len(matriz_container[i])):
             if (matriz_container[i][j] == ' '):
                 if (l < len(lista_string[s])):
                     matriz_container[i][j] = lista_string[s][l]
                     l += 1
-            
+
     # Redefine _tamanho_mod_ para o valor inicial
     _tamanho_mod_ = backup_tamanho
 
     return matriz_container
 
-## Imprime matriz
+## Retorna uma matriz que aglutina containers na linha seguinte do container anterior
+def comb_container(*matrizes_container):
+    m = matrizes_container[0]
+    
+    if len(matrizes_container) > 1:
+        for i in range(1, len(matrizes_container)):
+            m = m + matrizes_container[i]
+    
+    return m
+
+## Retorna uma lista de matrizes que um container pode ter e seu estado
+def divide_content(matriz_container):
+    #@todo fix, bug: adiciona espaços a cada corte (reorganizar o corte)
+    #@todo add '...'
+    global _tamanho_mod_
+
+    if (verificar_matriz_maior(matriz_container)):
+        m = []
+        end = 0
+
+        for i in range(0, len(matriz_container), _tamanho_mod_[0]):
+            if (i + _tamanho_mod_[0] < (len(matriz_container))):
+                end += _tamanho_mod_[0]
+            else: 
+                end = len(matriz_container)
+
+            m.append([False, matriz_container[i:end]])
+
+        m[0][0] = True
+        return m
+    
+    return ([[True, matriz_container]])
+
+## Altera a exibição atual do container ao pressionar qualquer tecla
+##@todo testagem maior
+def att_container(lista_matrizes):
+    for i in range(len(lista_matrizes)):
+        if (lista_matrizes[i][0] and (i < (len(lista_matrizes)-1))):
+            lista_matrizes[i][0] = False
+            lista_matrizes[i+1][0] = True
+
+## Retorna uma matriz com o desenho de uma tecla e uma label
+def tecla_com_label(tecla, label):
+    tamanho = len(tecla) + 4
+    m = container_text(container_retangulo(3, tamanho), tecla.center(tamanho-2))
+    m[1] += ' {}'.format(label)
+    return m
+
+## Imprime matriz simples
 def impressao_matriz(matriz):
-    pass
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            print(matriz[i][j],end= '')
+        
+        print()
+
+## Imprime matriz com múltiplas exibições
+##@todo testagem maior
+def impressao_matriz_m(lista_matrizes):
+    for i in range(len(lista_matrizes)):
+        if (lista_matrizes[i][0]):
+            for j in range(len(lista_matrizes[i][1])):
+                for k in range (len(lista_matrizes[i][1][j])):
+                    print(lista_matrizes[i][1][j][k], end=' ')
+                
+                print()
 
 ## Imprime toda a tela
-def impressao_tela(margem, alinhamento, matriz, init):
+##@todo função não implementada
+def impressao_tela(margem, alinhamento, init, matriz):
     alinhar_add_margem_tela(margem)
     
     match alinhamento:
         case 'c'|'centro'|'center'|'centralizar'|'centralizado':
             alinhar_centro(matriz)
+
         case 'd'|'direita'|'r'|'right':
             alinhar_direita(matriz)
+
         case 'e'|'esquerda'|'l'|'left':
             alinhar_esquerda(matriz)
+
         case 'j'|'justificar'|'justify'|'justificado':
             alinhar_justificar(matriz)
+
         case _:
             pass
-
-    # @todo tem que imprimir a tela inteira, exceto a matriz, dps a matriz
+        
     for i in range(_TAMANHO_[0]):
-        if (i == init):
-            print(matriz[i][j])
-        for j in range(_TAMANHO_[1]):
-                pass
+        if i == init:
+            impressao_matriz(matriz)
+            i += len(matriz)
+        else: 
+            for j in range(_TAMANHO_[1]):
+                print(' ')
+
+
+
         
 # Testes
 if __name__ == "__main__":
