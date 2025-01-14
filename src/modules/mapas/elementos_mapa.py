791e2d877ea7738235a1567f9549155d6242a290
Yasmim <108767728+Tybika@users.noreply.github.com>
2025-01-14 14:47:49 -0300

    refactor: changes data structure, uses only ASCII chars

--- src/modules/mapas/elementos_mapa.py
+++ src/modules/mapas/elementos_mapa.py
@@ -1,49 +1,59 @@
 # Importações
 import WConio2
 
-# Imprime o elemento e retorna à cor padrão
-def imprimir_elemento(elemento: tuple, cor_padrao = 15, *pos_set: int):
-    WConio2.textcolor(elemento[1])
-    if (type(elemento[0]) == str):
-        print(elemento[0], end='')
-    else:
-        print(elemento[0][pos_set[0]], end='')
+# Imprime o elemento e retorna à cor padrão 
+# se for um elemento com set, necessário informar posição 
+def imprimir_elemento(nome_elemento: str, cor_padrao = 15, pos_set = 0):
+    elementos = elementos_ASCII()
+
+    # Busca elemento e cor segundo nome
+    for i in range(len(elementos)):
+        if (elementos[i].get('nome') == nome_elemento):
+            # Verifica se o char é único ou é um set
+            if (type(elementos[i].get('char')) == str):
+                char = elementos[i].get('char')
+                cor = elementos[i].get('cor')
+                break
+            else:
+                char = elementos[i].get('char')[pos_set]
+                cor = elementos[i].get('cor')
+                break
+
+    WConio2.textcolor(cor)
+    print(char, end='')
     WConio2.textcolor(cor_padrao)
 
-
-# Definições dos elementos de mapa no padrão:
-#(caracter ou set de caracteres, cor WConio2)
-def main():
+# Retorna um array de definições dos elementos ASCII de mapa no padrão
+# {nome do elemento, caracter ou set de caracteres, cor WConio2}
+def elementos_ASCII():
     # Indicador do jogador
-    jogador = (['▲', '◄', '▼', '►'], 13)
+    j = dict(nome='jogador', char=['^', '<', 'v', '>'], cor= 13)
 
     # Elementos atravessáveis
     ## Terrenos
-    vazio = (' ', 15) 
-    água = ('≈', 11) 
-    mato_alto = ('Ж', 2) 
-    mato_baixo = ('ж', 10) 
-    areia = ('₪', 14) 
-
+    a1 = dict(nome='vazio', char=' ', cor= 15)
+    a2 = dict(nome='agua', char='~', cor= 11)
+    a3 = dict(nome='mato_alto', char='M', cor= 2)
+    a4 = dict(nome='mato_baixo', char='m', cor= 10)
+    a5 = dict(nome='areia', char='*', cor= 6)
+ 
     ## Elementos com eventos
-    porta = ('∩', 15) 
-    portal = ('۞', 15) 
+    a6 = dict(nome='porta', char='A', cor= 15)
+    a7 = dict(nome='portal', char='O', cor= 15)
 
 
     # Elementos intransponíveis
     ## Limites
-    parede = ('▓', 4) 
-    cerca = (['═', '║', '╔', '╗'], 4) 
+    i1 = dict(nome='parede', char='%', cor= 4)
+    i2 = dict(nome='cerca', char=['_', '|'], cor= 4)
 
     ## Elementos visuais de área
     ## Cemitério -> área de vazio com lápides
     ## Caverna -> área de vazio com pedras
     ## Floresta -> área de vazio com árvores
-    lapide = ('†', 7) 
-    pedra = ('⌂', 8) 
-    arvore = ('♣', 10) 
+    i3 = dict(nome='lapide', char='+', cor= 7)
+    i4 = dict(nome='pedra', char=',', cor= 8)
+    i5 = dict(nome='arvore', char='T', cor= 10)
 
-
-if __name__ == '__main__':
-    main()
-
+    elementos = [j, a1, a2, a3, a4, a5, a6, a7, i1, i2, i3, i4, i5]
+    return elementos
