217a491200a3665bc8aad6a02a4e164ee4f23883
AgathaLice <miguelfabrisd@gmail.com>
2025-01-20 10:47:37 -0300

    Update timer.py
    Correção em timer, agora pode ser implementado, falta adicionar winsound e limpar as linhas de teste.

--- src/utils/timer.py
+++ src/utils/timer.py
@@ -1,5 +1,5 @@
 from multiprocessing import Process, Value
-from time import sleep
+from time import sleep as t_Sleep
 from math import floor
 
 #! Função para traduzir minutos para segundos
@@ -16,20 +16,25 @@ def segundo_Para_Minuto(tempo): # Tem como parâmetro o tempo a ser traduzido
 #! Esse é o timer
 def timer(num):
     num.value = 180 #! Três minutos
-    while num.value > 0:
-        sleep(1)
+    while num.value >= 1:
+        t_Sleep(1)
         print(segundo_Para_Minuto(num.value)) ####!Essa linha é para teste, não é necessária o print, apenas a chamada da função, caso queira testar o timer, modifique o valor de segundos abaixo e retire o "#" do começo da linha
         num.value -= 1
-    return False
+    if num.value <= 1:
+        t_Sleep(1)
+        terminar_Timer()
+
+def get_NumValue():
+    return num.value
 
 #! Função main, cuidado
 def timer_Main():
     processo_Timer.start()
-#    processo_Timer.join() #!Também linhas para teste
-#    return False
 
-def zerar_Timer(timer):
-    timer.terminate()
+
+def terminar_Timer():
+#    timer.terminate()
+    num.value = 0
     return False
 
 #! Globais
@@ -38,9 +43,3 @@ processo_Timer = Process(target=timer, args=(num,))
 
 if __name__ == "__main__": # Necessário para o Processing ocorrer em Windows
     timer_Main()
-    while True:
-        print(segundo_Para_Minuto(num.value)) #! Comentário para teste, quando implementar o timer, deve ser removido
-        if num.value < 170:
-            break
-
-
