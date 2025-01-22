d05319779bff4163849257f6abf8c5eba4c55ba7
AgathaLice <miguelfabrisd@gmail.com>
2025-01-22 08:44:50 -0300

    feat: pausar timer
    Adicionei a função para pausar o timer

--- src/utils/timer.py
+++ src/utils/timer.py
@@ -21,26 +21,40 @@ def timer(num):
         t_Sleep(1)
         print(segundo_Para_Minuto(num.value)) ####!Essa linha é para teste, não é necessária o print, apenas a chamada da função, caso queira testar o timer, modifique o valor de segundos abaixo e retire o "#" do começo da linha
         num.value -= 1
-    if num.value <= 1:
+        while num.value <= -2:
+            t_Sleep(1)
+    if num.value <= 1 and num.value >= -1:
         t_Sleep(1)
         terminar_Timer()
 
 def get_NumValue():
     return num.value
 
+#def set_NumValue(numero):
+#    num.value = numero
+
 #! Função main, cuidado
 def timer_Main():
     processo_Timer.start()
 
+def pause_Timer():
+    global valor_Pausado
+    valor_Pausado = num.value
+    num.value = -3
+
+def despause_Timer():
+    global valor_Pausado
+    num.value = valor_Pausado
 
 def terminar_Timer():
     num.value = 0
-    winsound.Beep(700, 800)
+#    winsound.Beep(700, 800)
     return False
 
 #! Globais
 num = Value('i', 180) #! Três minutos
 processo_Timer = Process(target=timer, args=(num,))
+valor_Pausado = 0
 
 if __name__ == "__main__": # Necessário para o Processing ocorrer em Windows
     timer_Main()
